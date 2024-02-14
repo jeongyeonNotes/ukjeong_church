f9447e75b62823250356a128f5e07218b752552e
이정연 <devjy@ijeong-yeon-ui-Macmini.local>
2024-02-14 16:27:37 +0900

    HomeController Path update

--- src/main/java/com/ukjeong/ukjeong_churchweb/HomeController.java
+++ src/main/java/com/ukjeong/ukjeong_churchweb/HomeController.java
@@ -1,4 +1,4 @@
-package com.ukjeong.ukjeong_churchweb.Home;
+package com.ukjeong.ukjeong_churchweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
