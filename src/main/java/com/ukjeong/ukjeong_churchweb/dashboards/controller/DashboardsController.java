999ed03d7bcc778512e4f48e63e0bd636df3184b
jeongyeonNotes <devjy@ijeong-yeon-ui-Macmini.local>
2025-01-13 16:24:10 +0900

    25.01.13
    html 수정

--- src/main/java/com/ukjeong/ukjeong_churchweb/dashboards/controller/DashboardsController.java
+++ src/main/java/com/ukjeong/ukjeong_churchweb/dashboards/controller/DashboardsController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/finance")
 public class DashboardsController {
-
-    @RequestMapping(value = "/dashboards/index")
+    /* app-재정관리-메인 */
+    @RequestMapping(value = "/index")
     public String ViewDashboards(){
         System.out.println("move page test ....");
-        return "/dashboards/projects";
+        return "finance/financeIndex";
     }
+    /* app-재정관리-수입/지출 */
+    @RequestMapping(value = "/addingAccount")
+    public String ViewAddingAccount(){
+        System.out.println("move page test ....");
+        return "finance/addingAccount";
+    }
+
 }
